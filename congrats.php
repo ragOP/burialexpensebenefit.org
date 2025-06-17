64de8a57590c9608ab44de842fb1291e6da4da0f
ragOP <najmiraghib@gmail.com>
2025-06-17 21:43:42 +0530

    first commit

---
+++ congrats.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Congratulations</title>
+  <link rel="stylesheet" href="stylepage.css">
+<script src="https://b-js.ringba.com/CAfdb7eb5bdaa24fb8a155f293ece397ae" async></script>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WR6S38QQ');</script>
+<!-- End Google Tag Manager -->
+
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR6S38QQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <div class="checking" id="congratulations">
+    <div class="congrats">Congratulations, You Qualify 🎉 </div>
+    <div class="top-description-5">
+      Make A <b>Quick Call</b> To Claim Your $25,000 Life Insurance Benefit Now!  
+    </div>
+    <div class="spots-count">Spots remaining: 4</div>
+   
+
+    <div class="call-btn" onclick="handleCall()">
+  <a href="tel:+13214858035">CALL (321) 485-8035</a>
+</div>
+
+    <div class="sub-description">
+      Due to high call volume, your official agent is waiting for only <b>3 minutes</b>, then your spot will not be reserved.
+    </div>
+    <div class="timer">
+      <div class="timer-cell" id="minutes">3</div>
+      <div class="timer-cell">:</div>
+      <div class="timer-cell" id="seconds">00</div>
+    </div>
+  </div>
+</body>
+</html>
